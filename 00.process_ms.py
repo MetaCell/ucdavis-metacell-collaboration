ef2aa59a14e52f696e124475e3cf06095e20ba40
phildong <35715936+phildong@users.noreply.github.com>
2024-06-04 17:35:11 -0400

    update paths and misc for processing scripts

--- 00.process_ms.py
+++ 00.process_ms.py
@@ -24,11 +24,11 @@ from minian.utilities import TaskAnnotation
 from routine.minian_pipeline import minian_process
 
 IN_DPATH = "./data"
-IN_MINIAN_INT_PATH = "~/var/miniscope2s-validation/minian_int"
-IN_WORKER_PATH = "~/var/miniscope2s-validation/dask-worker-space"
+IN_MINIAN_INT_PATH = "~/var/minian-process/minian_int"
+IN_WORKER_PATH = "~/var/minian-process/dask-worker-space"
 IN_PARAM_FOLDER = "./process_parameters/"
-IN_SSMAP = "./data/sessions.csv"
-PARAM_SKIP_EXSISTING = True
+IN_SSMAP = "./sessions.csv"
+PARAM_SKIP_EXSISTING = False
 OUT_ERR_FOLDER = "./process_error"
 OUT_PATH = "./intermediate/processed/"
 FIG_FOLDER = "./figs/processed/"
@@ -60,9 +60,13 @@ if __name__ == "__main__":
     ssmap = pd.read_csv(IN_SSMAP)
     # process loop
     for _, ss_row in ssmap.iterrows():
-        dp, scp, ss = ss_row["data"], ss_row["scope"], ss_row["session"]
+        dp, anm, ss = (
+            os.path.dirname(ss_row["data"]),
+            ss_row["animal"],
+            ss_row["session"],
+        )
         if PARAM_SKIP_EXSISTING and os.path.exists(
-            os.path.join(OUT_PATH, "{}-{}.nc".format(scp, ss))
+            os.path.join(OUT_PATH, "{}-{}.nc".format(anm, ss))
         ):
             print("skipping {}".format(dp))
             continue
@@ -70,19 +74,19 @@ if __name__ == "__main__":
         with open(os.path.join(IN_PARAM_FOLDER, "generic.yaml")) as yf:
             param = yaml.full_load(yf)
         try:
-            with open(os.path.join(IN_PARAM_FOLDER, "{}.yaml".format(scp))) as yf:
+            with open(os.path.join(IN_PARAM_FOLDER, "{}.yaml".format(anm))) as yf:
                 param_anm = yaml.full_load(yf)
             param.update(param_anm)
-            print("using config {}.yaml".format(scp))
+            print("using config {}.yaml".format(anm))
         except FileNotFoundError:
             pass
         try:
             with open(
-                os.path.join(IN_PARAM_FOLDER, "{}-{}.yaml".format(scp, ss))
+                os.path.join(IN_PARAM_FOLDER, "{}-{}.yaml".format(anm, ss))
             ) as yf:
                 param_ss = yaml.full_load(yf)
             param.update(param_ss)
-            print("using config {}-{}.yaml".format(scp, ss))
+            print("using config {}-{}.yaml".format(anm, ss))
         except FileNotFoundError:
             pass
         shutil.rmtree(IN_MINIAN_INT_PATH, ignore_errors=True)
@@ -108,30 +112,31 @@ if __name__ == "__main__":
             tstart = datetime.now()
             with redirect_stdout(io.StringIO()):
                 result_ds, plots = minian_process(
-                    dpath=os.path.join(IN_DPATH, dp, "Miniscope"),
+                    dpath=os.path.join(IN_DPATH, dp),
                     intpath=IN_MINIAN_INT_PATH,
                     param=param,
+                    video_path=os.path.join(IN_DPATH, dp, "minian.mp4"),
                 )
             tend = datetime.now()
             print("minian success: {}".format(dp))
             print("time: {}".format(tend - tstart))
         except Exception as err:
             print("minian failed: {}".format(dp))
-            with open(os.path.join(OUT_ERR_FOLDER, "-".join([scp, ss])), "w") as txtf:
+            with open(os.path.join(OUT_ERR_FOLDER, "-".join([anm, ss])), "w") as txtf:
                 traceback.print_exception(None, err, err.__traceback__, file=txtf)
             client.close()
             cluster.close()
             continue
         result_ds = (
-            result_ds.assign_coords(scope=scp, session=ss)
+            result_ds.assign_coords(scope=anm, session=ss)
             .expand_dims(["scope", "session"])
             .compute()
         )
         result_ds.to_netcdf(
-            os.path.join(OUT_PATH, "{}-{}.nc".format(scp, ss)), format="NETCDF4"
+            os.path.join(OUT_PATH, "{}-{}.nc".format(anm, ss)), format="NETCDF4"
         )
         for plt_name, plt in plots.items():
-            plt_name = "-".join([scp, ss, plt_name])
+            plt_name = "-".join([anm, ss, plt_name])
             save(
                 hv.render(plt),
                 os.path.join(FIG_FOLDER, "{}.html".format(plt_name)),
